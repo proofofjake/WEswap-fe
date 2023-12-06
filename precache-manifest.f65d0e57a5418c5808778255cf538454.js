2a302c40a5b688a777774e7fdcb3773b923a59bf
proofofjake <jake.galvin@btinternet.com>
2023-12-06 02:45:06 +0000

    Updates

---
+++ precache-manifest.f65d0e57a5418c5808778255cf538454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8923beba8c244a0603c971dc1208d6a2",
+    "url": "/Alternative-Uniswap-Interface/index.html"
+  },
+  {
+    "revision": "84a8615066cbbadd4bf6",
+    "url": "/Alternative-Uniswap-Interface/static/css/2.0fe88a37.chunk.css"
+  },
+  {
+    "revision": "8f237036f5041ee207a3",
+    "url": "/Alternative-Uniswap-Interface/static/css/main.e21b4292.chunk.css"
+  },
+  {
+    "revision": "84a8615066cbbadd4bf6",
+    "url": "/Alternative-Uniswap-Interface/static/js/2.4785a8a4.chunk.js"
+  },
+  {
+    "revision": "fdf0b541f70965b133e0",
+    "url": "/Alternative-Uniswap-Interface/static/js/3.1d5d5963.chunk.js"
+  },
+  {
+    "revision": "8f237036f5041ee207a3",
+    "url": "/Alternative-Uniswap-Interface/static/js/main.00759bbf.chunk.js"
+  },
+  {
+    "revision": "9cce07cab5b6063bf164",
+    "url": "/Alternative-Uniswap-Interface/static/js/runtime~main.c4fb756c.js"
+  }
+]);
--- src/constants/chains.js
+++
@@ -1,8 +0,0 @@
-export const networks = [17001]
-
-export const ChainId = {
-  REDSTONE: 17001
-};
-
-export const routerAddress = new Map();
-routerAddress.set(ChainId.REDSTONE, "0x8bCa00Df3Ab70bf24718Bc9AEdd30Ed6062F0b0C");
