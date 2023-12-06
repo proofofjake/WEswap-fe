9a37cdfd16db010581178fb4930837300852f678
proofofjake <jake.galvin@btinternet.com>
2023-12-06 03:32:10 +0000

    Updates

---
+++ precache-manifest.8407216baf7d7264445cf619d1543b03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3969833eab34bc6ba5436a8b064efc30",
+    "url": "/WeSwapInterface/index.html"
+  },
+  {
+    "revision": "84a8615066cbbadd4bf6",
+    "url": "/WeSwapInterface/static/css/2.0fe88a37.chunk.css"
+  },
+  {
+    "revision": "8f237036f5041ee207a3",
+    "url": "/WeSwapInterface/static/css/main.e21b4292.chunk.css"
+  },
+  {
+    "revision": "84a8615066cbbadd4bf6",
+    "url": "/WeSwapInterface/static/js/2.4785a8a4.chunk.js"
+  },
+  {
+    "revision": "fdf0b541f70965b133e0",
+    "url": "/WeSwapInterface/static/js/3.1d5d5963.chunk.js"
+  },
+  {
+    "revision": "8f237036f5041ee207a3",
+    "url": "/WeSwapInterface/static/js/main.00759bbf.chunk.js"
+  },
+  {
+    "revision": "28f45fad33996df1b41f",
+    "url": "/WeSwapInterface/static/js/runtime~main.3e81a95d.js"
+  }
+]);
